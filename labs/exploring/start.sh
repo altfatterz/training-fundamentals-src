b6dcaedb308fb0a5a4f9d06420286059325d3a54
Zoltan Altfatter <zoltan.altfatter@sixdx.com>
2020-03-30 12:05:33 +0200

    cleanup exploring lab

--- labs/exploring/start.sh
+++ labs/exploring/start.sh
@@ -2,20 +2,28 @@
 
 docker-compose up -d
 
-echo "Waiting for Kafka to launch on 9092..."
-while ! nc -z kafka 9092; do   
+echo "Checking Kafka connection..."
+while ! nc -z localhost 19092; do
   sleep 1.0
   echo "Kafka not yet ready..."
 done 
 echo "Kafka is now ready!"
 
-kafka-topics --bootstrap-server kafka:9092 \
+echo "Creating vehicle-positions topic..."
+kafka-topics --bootstrap-server localhost:19092 \
     --topic vehicle-positions \
     --create \
     --partitions 6 \
     --replication-factor 1
 
+echo "Currently available topics:"
+kafka-topics --bootstrap-server localhost:19092 --list
+
+echo "Starting cnfltraining/vp-producer:v2 producer..."
 docker container run -d \
     --name producer \
     --net exploring_confluent \
     cnfltraining/vp-producer:v2
+
+echo "Running containers:"
+docker ps
